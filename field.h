5bbfb5cc57a63607a2ef33ce726c81b5ccf80dfe
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2023-12-15 22:25:28 +0300

    Update field.h

--- field.h
+++ field.h
@@ -3,9 +3,9 @@
 #ifndef FIELD_H
 #define FIELD_H
 
-void get_potential_field(std::vector<vector<double >>& scalar_field, Object object);
+void get_potential_field(std::vector<std::vector<double >>& scalar_field, Object object);
 
-void get_vector_field(std::vector<vector<double >>& vector_field, std::vector<vector<double >>& scalar_field);
+void get_vector_field(std::vector<std::vector<double >>& vector_field, std::vector<std::vector<double >>& scalar_field);
 
 void give_potential_field(Object object);
 
