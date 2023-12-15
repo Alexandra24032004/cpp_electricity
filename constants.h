5bd175069daa04a0f27c84873994b09d4b833f01
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2023-12-15 22:21:04 +0300

    Create constants.h

---
+++ constants.h
@@ -0,0 +1,13 @@
+#include <string>
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
+const int FIELD_WIDTH = 1000;
+const int FIELD_HEIGHT = 1000;
+const double ELECTRICAL_CONSTANT = 1.0;
+const std::string POTENTIAL_FIELD_FILE = "scalar_visualisation_data.txt";
+
+#endif
