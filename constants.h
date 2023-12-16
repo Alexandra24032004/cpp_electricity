b1ba5997e6c0221b66b7e4ee8c8e814d3a5ba518
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2023-12-16 06:11:43 +0300

    Update constants.h

--- constants.h
+++ constants.h
@@ -10,16 +10,16 @@ const int FIELD_HEIGHT = 1000;
 const double ELECTRICAL_CONSTANT = 1.0;
 const std::string POTENTIAL_FIELD_FILE = "scalar_visualisation_data.txt";
 const std::string VECTOR_FIELD_FILE = "vector_visualisation_data.txt";
-const int length_of_bold = 500; 
+const int length_of_bold = 500;
 const int widht_of_bold = 30;
 const int step = 30;
 int const null_x = 50;
 int const null_y = 50;
 const int shape_of_grid = 20;
-const int num_of_vec = shape_of_area/shape_of_grid;
-const string c1 = "#0000FF"; //blue
-const string c2 = "#FFFF00"; //yellow
-const string c3 = "#EE82EE"; //purple
-const string c4 = "#00FFFF"; //light blue
+const int num_of_vec = FIELD_WIDTH / shape_of_grid;
+const std::string c1 = "#0000FF"; //blue
+const std::string c2 = "#FFFF00"; //yellow
+const std::string c3 = "#EE82EE"; //purple
+const std::string c4 = "#00FFFF"; //light blue
 
 #endif
