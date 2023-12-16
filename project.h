7039fea8400714411b0eb2eaee454b4b4c21b748
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2023-12-16 07:32:03 +0300

    Update project.h

--- project.h
+++ project.h
@@ -1,17 +1,23 @@
 #include "constants.h"
 #include "object.h"
 
-#ifndef OBJECT_H
-#define OBJECT_H
+
+#ifndef PROJECT_H
+#define PROJECT_H
 
 class Project
 {
 private:
-	std::vector<std::vector<double>> scalar_field(SCREEN_HEIGHT, std::vector<double>(SCREEN_WIDTH, 0));
+	std::vector<std::vector<double>> scalar_field;  
 	std::vector<Object> objects;
 	bool is_scalar = 1;
 
 public:
+
+	Project()
+	{
+		scalar_field = std::vector<std::vector<double>>(SCREEN_HEIGHT, std::vector<double>(SCREEN_WIDTH, 0));
+	}
 
 	void run();
 };
