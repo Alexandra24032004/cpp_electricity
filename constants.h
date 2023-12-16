da91137014487e2ff042615e944c7e0b1200bbb4
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2023-12-16 05:00:16 +0300

    Update constants.h

--- constants.h
+++ constants.h
@@ -3,12 +3,17 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int SCREEN_WIDTH = 1920;
-const int SCREEN_HEIGHT = 1080;
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = 1000;
 const int FIELD_WIDTH = 1000;
 const int FIELD_HEIGHT = 1000;
 const double ELECTRICAL_CONSTANT = 1.0;
 const std::string POTENTIAL_FIELD_FILE = "scalar_visualisation_data.txt";
 const std::string VECTOR_FIELD_FILE = "vector_visualisation_data.txt";
+const int length_of_bold = 500; 
+const int widht_of_bold = 30;
+const int step = 30;
+int const null_x = 50;
+int const null_y = 50;
 
 #endif
