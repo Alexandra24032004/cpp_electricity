37e4e451e6071c809d3ff417617c6d80d4932d69
Alexandra24032004 <74011794+Alexandra24032004@users.noreply.github.com>
2023-12-16 05:47:48 +0300

    Create visual.h

---
+++ visual.h
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp> //for visualisation
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include <fstream> //for work with files
+#include <string>  //for work with strings
+#include <sstream>
+#include <iomanip>
+#include <tuple> //for work with turples
+
+#ifndef VISUAL_H
+#define VISUAL_H
+
+void tokenize(string const &str, const char delim, vector<string> &out);
+
+string fadeColor(string c1, string c2, double mix = 0);
+
+tuple<int, int, int> convertColor(const string& colorCode);
+
+vector<vector<vector<int>>> coloring(const vector<vector<double>>& arr, double max_el, const string& c1, const string& c2);
+
+void display_scalar_field(const vector<vector<vector<int>>>& arr);
+
+vector<vector<vector<int>>> get_scalar_field_ff ();
+
+vector<Vec> get_vector_field_ff (vector<int> &body);
+
+void display_vector_field (vector<Vec> & vectors, vector<int> &body);
+
+void visualisation (vector<vector<double>> &scalar_field, bool is_scalar, vector<Object> &objects);
+
+#endif
