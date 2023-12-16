271eceea1e3ec91ced8ea79b4fe245457c885431
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2023-12-16 05:48:18 +0300

    Update object.cpp

--- object.cpp
+++ object.cpp
@@ -1,21 +1,27 @@
 #include "constants.h"
 #include "object.h"
 #include "point.h"
+#include "parse.h"
+#include <symengine/symbol.h>
 
-Object::Object()
+
+Object::Object(std::vector<std::vector<double >>& scalar_field, std::vector<std::string>& strings, int _x, int _y, bool is_scalar)
 {
-	//Formula..;
+	shift_x = _x;
+	shift_y = _y;
 
 	for (int y = 0; y < FIELD_HEIGHT; y++)
 	{
-		/*for (int x = 0; x < FIELD_WIDTH; x++)
+		for (int x = 0; x < FIELD_WIDTH; x++)
 		{
-			if (is_in_figure(formula))
+			if (calc_all_expressions(strings, shift_x - x, shift_y - y))
 			{
 				points_array.push_back(Point(x, y));
 			}
-		}*/
+		}
 	}
+
+	visualize(scalar_field, is_scalar);
 }
 
 double Object::get_potential_in_the_point(int _x, int _y)
