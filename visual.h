2f114a7fa978043e20d07772c95c068c351d18e7
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2023-12-16 07:31:18 +0300

    Update visual.h

--- visual.h
+++ visual.h
@@ -7,26 +7,30 @@
 #include <sstream>
 #include <iomanip>
 #include <tuple> //for work with turples
+#include "Vec.h" 
+#include "object.h" 
+
+using namespace std;
 
 #ifndef VISUAL_H
 #define VISUAL_H
 
 void tokenize(string const &str, const char delim, vector<string> &out);
 
-string fadeColor(string c1, string c2, double mix = 0);
+string fadeColor(string c1, string c2, double mix = 0.0);
 
 tuple<int, int, int> convertColor(const string& colorCode);
 
 vector<vector<vector<int>>> coloring(const vector<vector<double>>& arr, double max_el, const string& c1, const string& c2);
 
-void display_scalar_field(const vector<vector<vector<int>>>& arr);
+void display_scalar_field(const vector<vector<vector<int>>>& arr, sf::RenderWindow& window);
 
 vector<vector<vector<int>>> get_scalar_field_ff ();
 
 vector<Vec> get_vector_field_ff (vector<int> &body);
 
-void display_vector_field (vector<Vec> & vectors, vector<int> &body);
+void display_vector_field (vector<Vec> & vectors, vector<int> &body, sf::RenderWindow& window);
 
-void visualisation (vector<vector<double>> &scalar_field, bool is_scalar, vector<Object> &objects);
+void visualisation (vector<vector<double>> &scalar_field, bool is_scalar, vector<Object> &objects, sf::RenderWindow& window);
 
 #endif
